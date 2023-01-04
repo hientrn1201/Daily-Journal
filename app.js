c84c1b3bd9253a666f0cbfb900e0f78c187605a7
Hien Tran <85942569+hientrn1201@users.noreply.github.com>
2023-01-04 17:01:34 +0700

    update database

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "Welcome to our daily journal! Here, you can find daily updates and musings about a variety of topics, ranging from personal reflections to current events.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -16,15 +17,33 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const posts = [];
+
+//connect to mongoDB
+const url = "mongodb://127.0.0.1:27017/blogDB"
+mongoose.set("strictQuery", false);
+mongoose.connect(url,  {useNewUrlParser: true});
+
+//create posts schema and collection in mongoDB
+const postsSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  }
+});
+const Post = mongoose.model("Post", postsSchema);
 
 app.get("/", (req, res) => {
-  const options = {
-    homeStartingContent: homeStartingContent,
-    posts: posts,
-  }
-
-  res.render("home", options);
+  Post.find({}, (err, foundedPosts) => {
+    const options = {
+      homeStartingContent: homeStartingContent,
+      posts: foundedPosts,
+    };
+    res.render("home", options);
+  });
 })
 
 app.get("/about", (req, res) => {
@@ -48,20 +67,24 @@ app.get("/compose", (req, res) => {
 })
 
 app.post("/compose", (req, res) => {
-  const post = {
+  const post = new Post({
     title: req.body.title,
     content: req.body.postContent,
-  }
-  posts.push(post);
-  res.redirect("/");
+  });
+  post.save((err) => {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 })
 
-app.get("/posts/:title", (req, res) => {
-  posts.forEach((post) => {
-    if (_.lowerCase(post.title) === _.lowerCase(req.params.title)) { 
-      res.render("post", post); 
-    } 
-  })
+app.get("/posts/:id", (req, res) => {
+  Post.findById(req.params.id, (err, foundedPost) => {
+    if (!err) {
+      console.log(foundedPost);
+      res.render("post", {foundedPost: foundedPost});
+    }
+  });
 })
 
 
