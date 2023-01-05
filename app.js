7cdf6d498309e64013956ec99a6a5dbc57fb1539
Hien Tran <85942569+hientrn1201@users.noreply.github.com>
2023-01-05 22:37:48 +0700

    added date

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const _ = require("lodash");
 const mongoose = require("mongoose");
 
 const homeStartingContent = "Welcome to our daily journal! Here, you can find daily updates and musings about a variety of topics, ranging from personal reflections to current events.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
@@ -32,7 +30,8 @@ const postsSchema = mongoose.Schema({
   content: {
     type: String,
     required: true
-  }
+  },
+  time : String,
 });
 const Post = mongoose.model("Post", postsSchema);
 
@@ -46,36 +45,29 @@ app.get("/", (req, res) => {
   });
 })
 
-app.get("/about", (req, res) => {
-  const options = {
-    aboutContent: aboutContent,
-  }
-
-  res.render("about", options);
-})
-
-app.get("/contact", (req, res) => {
-  const options = {
-    contactContent: contactContent,
-  }
-
-  res.render("contact", options);
-})
-
 app.get("/compose", (req, res) => {
   res.render("compose");
 })
 
 app.post("/compose", (req, res) => {
+  let today = new Date();
+  const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+  };
+  let day = today.toLocaleDateString("en-US", options);
+
   const post = new Post({
     title: req.body.title,
     content: req.body.postContent,
+    time: day
   });
   post.save((err) => {
     if (!err) {
       res.redirect("/");
     }
-  });
+  }); 
 })
 
 app.get("/posts/:id", (req, res) => {
